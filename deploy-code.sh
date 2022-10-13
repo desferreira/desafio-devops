be8df76da2f475dccaf05602dc6cbf37c459b3e2
Diego Ferreira <diegoferreira4@hotmail.com>
2022-10-13 12:29:49 +0100

    :sparkles: Add build and push for docker images

---
+++ deploy-code.sh
@@ -0,0 +1,17 @@
+# # Deploying infrastructure
+cd infra-as-code
+terraform apply -auto-approve
+
+export VM_PUBLIC_IP=$(terraform output -raw server_public_ip)
+export MYSQL_DB_HOST=$(terraform output -raw db_url)
+export S3_BUCKET_ADDRESS=$(terraform output -raw s3_bucket_address)
+
+# Deploying backend
+cd ../backend
+docker build -t dig0w/letscode_be .
+docker push dig0w/letscode_be
+
+# Deploying frontend
+cd ../frontend
+docker build -t dig0w/letscode_fe . 
+docker push dig0w/letscode_fe
--- infra-as-code/scripts/startup.sh
+++ infra-as-code/scripts/startup.sh
@@ -13,3 +13,11 @@ sudo yum-config-manager \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo systemctl start docker
+
+# Pull docker images
+docker pull dig0w/letscode_fe
+docker pull dig0w/letscode_be
+
+# Running docker images
+docker run -p 80:80 dig0w/letscode_fe
+docker run -p 8080:8080 -p 443:443 -e MYSQL_DB_HOST=MYSQL_DB_HOST -e MYSQL_DB_USER=letscode -e MYSQL_DB_PASS="7ROtBB44*0XN" dig0w/letscode_be
